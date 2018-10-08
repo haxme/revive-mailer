8a891389dc99b42c898a605a2cb9c7cbe8f46918
Curran Higgins <curran@schooldata.net>
2018-10-08 08:57:27 -0700

    Start config
    Start laying out the project by defining a configuration file
    and setting up the gitignore. Required and optional fields will
    be determined by documentation instead of mass comments added
    to the config.

---
+++ config.default.php
@@ -0,0 +1,25 @@
+<?php
+
+DEFINE('FROM_ADDRESS', 'your-email@domain.tld');
+DEFINE('FROM_ADDRESS_PASSWORD', 'password-for-your-email');
+
+DEFINE('RECIPIENT_SET', []);
+DEFINE('RECIPIENT_SET_FILE', '/absolute/path/to/set.txt');
+DEFINE('RECIPIENT_SET_FILE_DELIMITER', ',');
+
+DEFINE('SMTP_SRV', 'smtp1.server.tld;smtp2.server.tld');
+DEFINE('SMTP_PORT', 587);
+
+DEFINE('IS_HTML', true);
+
+DEFINE('INTERSECTION_MODE', false);
+
+DEFINE('INTERSECTION_SET', []);
+DEFINE('INTERSECTION_SET_FILE', '/absolute/path/to/intersection-set.txt');
+DEFINE('INTERSECTION_SET_FILE_DELIMITER', ',');
+
+DEFINE('USE_INTERSECTION_DB', false);
+DEFINE('INTERSECTION_DB_HOST', '127.0.0.1');
+DEFINE('INTERSECTION_DB_NAME', 'name-of-database');
+DEFINE('INTERSECTION_DB_USER', 'name-of-db-user');
+DEFINE('INTERSECTION_DB_PASSWORD', 'password-for-name-of-database');
